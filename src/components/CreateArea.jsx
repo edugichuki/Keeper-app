bbb64c283ccdb718642cdb50957f9f41192fe6f7
edugichuki <muriithigichuki854@gmail.com>
2023-09-30 19:33:44 +0300

    Add functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
-function CreateArea() {
+const CreateArea = (props) => {
+  const [isExpanded, setExpanded] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue, // spread operator: spreads all of the key value pairs that are currently in existence in our note.
+        [name]: value, // this syntax simply turns this name key from just the string name for the key to the actual value of this name constant
+      };
+    });
+  };
+
+  const submitNote = (event) => {
+    props.onAdd(note);
+    setNote({ title: "", content: "" });
+    event.preventDefault();
+  };
+
+  const expand = () => {
+    setExpanded(true);
+  };
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 4 : 1}
+        />
+
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
-}
+};
 
 export default CreateArea;
